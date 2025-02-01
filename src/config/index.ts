f16eb0a34770cd463e57a9fd61d858967dcd839b
Hoon Kim <mail@hoonkim.me>
2025-02-01 14:42:13 +0100

    remove data folder

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-  perplexityApi: "", // only for testing. Do not commit
+  perplexityApi: "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", // only for testing. Do not commit
   mapsApi: "",
   perplexityEndpoint: "https://api.perplexity.ai",
   perplexityModel: "sonar-pro",
@@ -8,20 +8,54 @@ export const appConfig = {
 };
 
 export const supportedEventTypes = [
-    "Video Games",
-    "Blockchain",
-    "IT",
-    "AI",
-    "Investment",
-    "Real-Estate",
-    "Custom Event"
+  "Video Games",
+  "Blockchain",
+  "IT",
+  "AI",
+  "Investment",
+  "Real-Estate",
+  "Health",
+  "Music",
+  "Sports",
+  "IoT",
+  "Virtual Reality",
+  "Augmented Reality",
+  "Education",
+  "Finance",
+  "Marketing",
+  "Science",
+  "Engineering",
+  "Literature",
+  "Art",
+  "Travel",
+  "Food",
+  "Fashion",
+  "Environment",
+  "Politics"
 ]
 
 export const supportedEventLocations = [
-    "Germany",
-    "USA",
-    "Italy",
-    "Japan",
-    "Thailand",
-    "Custom Location"
+  "Germany",
+  "USA",
+  "Italy",
+  "Japan",
+  "Thailand",
+  "Australia",
+  "Canada",
+  "UK",
+  "France",
+  "Spain",
+  "Brazil",
+  "China",
+  "India",
+  "Russia",
+  "South Africa",
+  "Mexico",
+  "Argentina",
+  "Nigeria",
+  "Egypt",
+  "Saudi Arabia",
+  "UAE",
+  "Turkey",
 ]
+
