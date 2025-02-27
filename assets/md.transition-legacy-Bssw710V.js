6c69d604429d8a749a08995e79185e47cb92c0fb
Hoon Kim <mail@hoonkim.me>
2025-02-27 16:44:04 +0100

    Updates

--- assets/md.transition-legacy-Bssw710V.js
+++ assets/md.transition-legacy-Bssw710V.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-opcLt1bH.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BuUGxMg8.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
