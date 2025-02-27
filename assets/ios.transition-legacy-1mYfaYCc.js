5ed46fca44577149ac2afa2f6bc6d3b69e3eedf3
Hoon Kim <mail@hoonkim.me>
2025-02-27 20:18:32 +0100

    Updates

--- assets/ios.transition-legacy-1mYfaYCc.js
+++ assets/ios.transition-legacy-1mYfaYCc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BuUGxMg8.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DZ6yCVX1.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
