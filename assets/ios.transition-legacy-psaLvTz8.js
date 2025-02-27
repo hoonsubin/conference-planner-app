3bc2b9addb2d019d04d55bc95453387b668183bd
Hoon Kim <mail@hoonkim.me>
2025-02-27 16:34:48 +0100

    Updates

--- assets/ios.transition-legacy-psaLvTz8.js
+++ assets/ios.transition-legacy-psaLvTz8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B_6C_cyH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-opcLt1bH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
