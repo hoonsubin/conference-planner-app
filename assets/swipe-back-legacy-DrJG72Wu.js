8535fded687ef0be8d74fe0acd023be995a187a7
Hoon Kim <mail@hoonkim.me>
2025-02-27 20:22:33 +0100

    Updates

--- assets/swipe-back-legacy-DrJG72Wu.js
+++ assets/swipe-back-legacy-DrJG72Wu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DZ6yCVX1.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-D7dAJL0y.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
