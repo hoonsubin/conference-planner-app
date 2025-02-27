57b63f12dc6911fcf1592b883a5daff2b40a0e37
Kai <kai.m.schwabe@gmail.com>
2025-02-27 21:56:17 +0100

    Updates

--- assets/ios.transition-legacy-yZRM2aCX.js
+++ assets/ios.transition-legacy-yZRM2aCX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bo5GfWEw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-NEK5Xq66.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Dm3v3knz.js
+++ assets/swipe-back-legacy-Dm3v3knz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bo5GfWEw.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-NEK5Xq66.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
